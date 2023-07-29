1e96d65aeeec98b0e034c6f01c46a52c542081f8
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-07-29 12:38:13 +0600

    All carts reducer created

--- src/redux/reducers/cartSlice.ts
+++ src/redux/reducers/cartSlice.ts
@@ -3,9 +3,19 @@ import { IProduct } from "../../../constants";
 
 interface ICart {
   cpuCart: IProduct[];
+  ramCart: IProduct[];
+  psuCart: IProduct[];
+  sdCart: IProduct[];
+  monitorCart: IProduct[];
+  othersCart: IProduct[];
 }
 const initialState: ICart = {
   cpuCart: [],
+  ramCart: [],
+  psuCart: [],
+  sdCart: [],
+  monitorCart: [],
+  othersCart: [],
 };
 const cartSlice = createSlice({
   name: "cart",
@@ -17,8 +27,51 @@ const cartSlice = createSlice({
     removeFromCpuCart: (state) => {
       state.cpuCart = [];
     },
+    addToRamCart: (state, action) => {
+      state.ramCart = [action.payload];
+    },
+    removeFromRamCart: (state) => {
+      state.ramCart = [];
+    },
+    addToPsuCart: (state, action) => {
+      state.psuCart = [action.payload];
+    },
+    removeFromPsuCart: (state) => {
+      state.psuCart = [];
+    },
+    addToSdCart: (state, action) => {
+      state.sdCart = [action.payload];
+    },
+    removeFromSdCart: (state) => {
+      state.sdCart = [];
+    },
+    addToMonitorCart: (state, action) => {
+      state.monitorCart = [action.payload];
+    },
+    removeMonitorSdCart: (state) => {
+      state.monitorCart = [];
+    },
+    addToOthersCart: (state, action) => {
+      state.othersCart = [action.payload];
+    },
+    removeOthersSdCart: (state) => {
+      state.othersCart = [];
+    },
   },
 });
 
-export const { addToCpuCart, removeFromCpuCart } = cartSlice.actions;
+export const {
+  addToCpuCart,
+  removeFromCpuCart,
+  addToRamCart,
+  removeFromRamCart,
+  addToPsuCart,
+  removeFromPsuCart,
+  addToSdCart,
+  removeFromSdCart,
+  addToMonitorCart,
+  removeMonitorSdCart,
+  addToOthersCart,
+  removeOthersSdCart,
+} = cartSlice.actions;
 export default cartSlice.reducer;
